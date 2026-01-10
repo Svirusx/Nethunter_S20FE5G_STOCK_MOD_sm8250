50eb06efdd1f44440c51436a3f1f0ead0f67604c
Svirusx <45526199+Svirusx@users.noreply.github.com>
2026-01-10 20:48:06 +0100

    fix sched-pelt.h and cpufreq_schedutil.c compilation errors

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -879,8 +879,8 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	return count;
 }
 
-static struct governor_attr up_rate_limit_us = __ATTR_RW(up_rate_limit_us);
-static struct governor_attr down_rate_limit_us = __ATTR_RW(down_rate_limit_us);
+static struct governor_attr up_rate_limit_us __maybe_unused = __ATTR_RW(up_rate_limit_us);
+static struct governor_attr down_rate_limit_us __maybe_unused = __ATTR_RW(down_rate_limit_us);
 
 #ifdef CONFIG_SCHED_FFSI_GLUE
 static ssize_t fb_legacy_show(struct gov_attr_set *attr_set, char *buf)
